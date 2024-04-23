9ec5465bed153cf378121b9b6f6312c3f929612a
RaghunandhanG <143869346+RaghunandhanG@users.noreply.github.com>
2024-04-23 23:52:06 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title('CGPA CALCI')
+
+no_of_sub = st.number_input("Select the number of Subjects....", value=1, placeholder="Type a number..." , step = 1)
+total_credits = 0
+total = 0
+
+
+if no_of_sub is not None:
+    for i in range(int(no_of_sub)):
+        
+        credits = st.slider(f'Credits of the Subject {i+1}', 1, 4)
+        grade_pts = st.slider(f'Grade points of the Subject {i+1}', 6, 10)
+        
+        
+        total_credits += credits
+        total += credits * grade_pts
+        
+        
+    cgpa = total / total_credits
+
+
+    st.metric(label="CGPA", value=round(cgpa , 2) )
+    
